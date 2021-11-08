e16993129136ae1434da0c2222ecf22db40677bc
Oscar Izquierdo <33345036+OscarIzquierdo@users.noreply.github.com>
2021-11-08 13:44:05 +0100

    First Commit

---
+++ GameOff_2021/Assets/Test.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public enum enemigos {limo, avispa, murcielago, esqueleto};
+
+    public enemigos tipoEnemigo = enemigos.limo;
+}
