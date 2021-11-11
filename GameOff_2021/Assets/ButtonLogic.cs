0dacad2f827d5234d7890dc9eae89b01d9e08ce5
Oscar Izquierdo <33345036+OscarIzquierdo@users.noreply.github.com>
2021-11-11 12:56:41 +0100

    MainMenu

---
+++ GameOff_2021/Assets/ButtonLogic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonLogic : MonoBehaviour
+{
+    public enum sceneManagement {loadScene, quitGame}
+
+    public sceneManagement whatToDo;
+
+    [SerializeField] string levelToLoad;
+
+    public void ExecuteAction()
+    {
+        if (whatToDo == sceneManagement.loadScene)
+        {
+            SceneManager.LoadScene(levelToLoad);
+        }
+        else if (whatToDo == sceneManagement.quitGame)
+        {
+            Application.Quit();
+        }
+    }
+}
