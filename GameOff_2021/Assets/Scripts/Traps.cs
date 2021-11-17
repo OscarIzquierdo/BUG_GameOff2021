48f127c031a14219b62f81c6b1cd2fada2e1eb0c
AlbertoAnsar <albertoansar99@gmail.com>
2021-11-17 20:14:47 +0100

    traps

---
+++ GameOff_2021/Assets/Scripts/Traps.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Traps : MonoBehaviour
+{
+    public enum traps { axe, spikes };
+    public traps trapType;
+
+
+    [SerializeField] float timeToChangeSide;
+    private float maxTimeToChangeSide = 1.5f;
+
+    private bool isTopRight = false;
+    private bool isTopLeft = true;
+
+
+
+    [SerializeField] float timeToGoUp;
+    private float maxTimeToGoUp = 1.5f;
+    [SerializeField] float timeToGoDown;
+    private float maxTimeToGoDown = 0.5f;
+
+    private bool isUp;
+    private bool isDown;
+
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (trapType == traps.axe)
+        {
+            AxeTrapBehaviour();
+        }
+        else if (trapType == traps.spikes)
+        {
+            SpikeTrapBehaviour();
+        }
+    }
+
+    void AxeTrapBehaviour()
+    {
+        timeToChangeSide += Time.fixedDeltaTime;
+
+        if (timeToChangeSide >= maxTimeToChangeSide && isTopLeft)
+        {
+            isTopLeft = false;
+            isTopRight = true;
+        }
+
+        else if (timeToChangeSide >= maxTimeToChangeSide && isTopRight)
+        {
+            isTopLeft = true;
+            isTopRight = false;
+        }
+    }
+
+    void SpikeTrapBehaviour()
+    {
+
+    }
+}
